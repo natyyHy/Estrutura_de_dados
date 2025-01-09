3c9abd3c0ea26468a48f2e85822aa56e9ebccf78
Natiele Grazielly <159733563+natyyHy@users.noreply.github.com>
2025-01-09 09:17:51 -0300

    Add files via upload

---
+++ trabalho_II_listaDupla/menu.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void menu() {
+    cout << "\n\t>>> MENU LISTA DE ALUNOS DUPLAMENTE LIGADA <<<" << endl;
+    cout << "\t1 - Inserir no inicio" << endl;
+    cout << "\t2 - Inserir no final" << endl;
+    cout << "\t3 - Remover aluno procurado" << endl;
+    cout << "\t4 - Mostrar lista do inicio ao fim" << endl;
+    cout << "\t5 - Mostrar lista do fim ao inicio" << endl;
+    cout << "\t0 - Sair" << endl;
+    cout << "\t--------------------------" << endl;
+}
