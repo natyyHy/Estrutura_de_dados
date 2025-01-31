75d27681911a1aa31a127b7f4d39bb3e43c406f1
natyyHy <natielegrazielly5@gmail.com>
2025-01-31 11:07:20 -0300

    :feat:feat: criando buscar linear

---
+++ ordenacao_e_busca/buscalinear.c
@@ -0,0 +1,11 @@
+
+//isso e um teste
+
+void busca_linar(int v[],int a,int n){
+    for(int i = 0; i < n; i++){
+        if(v[i] == a){
+            return i; //posicao
+        }
+    }
+    return -1; //posicao inesxistente
+}
